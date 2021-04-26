e3e63e1f6e74a7e21cf4d682a79119e3096ec3dd
peax3 <peaceadjerese@gmail.com>
2021-04-26 15:36:14 +0100

    add: routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
+import SignIn from "./components/SignIn";
+import Signup from "./components/Signup";
 import Layout from "./components/Layout";
 
 function App() {
   return (
     <div className="App">
-      <Layout />
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/signin" />} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/contacts" component={Layout} />
+      </Switch>
     </div>
   );
 }
